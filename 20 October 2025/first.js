73d238d70ce38ed3c7c5c6f2af774f427e0084c4
karvind08 <karvind08@gmail.com>
2025-10-20 13:12:00 +0530

    20 october 2025

--- 20 October 2025/first.js
+++ 20 October 2025/first.js
@@ -1,1 +1,7 @@
 console.log("Hello Javascript!");
+console.log("Hi Arvind");
+console.log("Hey, I love Javascript!");
+age = 24
+console.log('The age is',age)
+name = "Arvind"
+console.log('The name is ',name)
