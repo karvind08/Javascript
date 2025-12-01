5513ebeb3aa7cb449226ddc15cfff89c8a412b01
karvind08 <karvind08@gmail.com>
2025-12-01 23:06:39 +0530

    01 December 2025

--- String/string1.js
+++ String/string1.js
@@ -7,3 +7,13 @@ console.log('Hello',name1)
 
 let name2 = 'Hello'
 console.log(name2[0]);
+console.log(name2.length);
+console.log(name2[name2.length-1]);
+
+console.log('String Conversion');
+let status = false;
+console.log(status);
+let str = status.toString(); // "false"
+console.log(str);
+let back = Boolean(str); // true
+console.log(back);
